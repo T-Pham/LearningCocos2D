db1582bed962eaec7c50c76e8db77f539e4956fd
Thanh Pham <thanh.pham@eastagile.com>
2013-08-05 11:19:17 +0700

    Initial commit

---
+++ HelloWorld/HelloWorld/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  HelloWorld
+//
+//  Created by eastagile 87:21 on 8/5/13.
+//  Copyright eastagile 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by eastagile 87:21 on 8/5/13.
+//  Copyright eastagile 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
