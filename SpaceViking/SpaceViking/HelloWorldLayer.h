b7041d09f5694ab934c3ba89c9c8f02827c4c121
Thanh Pham <thanh.pham@eastagile.com>
2013-08-05 15:12:48 +0700

    Chapter 2 initial commit

---
+++ SpaceViking/SpaceViking/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  SpaceViking
+//
+//  Created by eastagile 87:21 on 8/5/13.
+//  Copyright eastagile 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
